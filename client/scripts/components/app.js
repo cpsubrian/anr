0cc4f6b350479d10f617c856e5ce19d72c10d8e7
Brian Link <cpsubrian@gmail.com>
2015-02-08 14:23:38 -0800

    Sets up basic react-hot-loader.

---
+++ client/scripts/components/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var React = require('react');
+
+var App = React.createClass({
+
+  getInitialState() {
+    return {
+      clicked: false
+    };
+  },
+
+  buttonStyle() {
+    return {
+      color: '#fff',
+      backgroundColor: this.state.clicked ? 'red' : 'cyan',
+      padding: 20
+    };
+  },
+
+  onClick(e) {
+    e.preventDefault();
+    this.setState({clicked: !this.state.clicked});
+  },
+
+  render() {
+    return (
+      <section className="App">
+        <h1>Hello, world.</h1>
+        <p>This is my paragraph text.</p>
+        <button style={this.buttonStyle()} onClick={this.onClick}>Click Me</button>
+      </section>
+    );
+  }
+});
+
+module.exports = App;

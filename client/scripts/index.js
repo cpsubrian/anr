807013c47280c500ae865900729aef3584983869
Brian Link <cpsubrian@gmail.com>
2015-02-08 16:08:51 -0800

    Gets style loader working.

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -3,4 +3,6 @@
 var React = require('react'),
     App = require('./components/app');
 
+require('styles/app.css');
+
 React.render(<App />, document.body);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,26 +1,34 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
+  , path = require('path');
 
 module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    __dirname + '/scripts/index'
+    './scripts/index'
   ],
   output: {
-    path: __dirname + '/scripts/',
-    filename: 'bundle.js',
-    publicPath: '/scripts/'
+    path: __dirname,
+    filename: 'scripts/bundle.js',
+    publicPath: '/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   resolve: {
-    extensions: ['', '.js']
+    fallback: [
+      __dirname + '/bower_componenets'
+    ],
+    alias: {
+      styles: __dirname + '/styles'
+    },
+    extensions: ['', '.js', '.css']
   },
   module: {
     loaders: [
+      { test: /\.css$/, loaders: ['style', 'css'] },
       { test: /\.js$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
     ]
   }

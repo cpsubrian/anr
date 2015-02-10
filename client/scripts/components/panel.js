3de9f2959c9acb45c1ce9d345d6d69966620b517
Brian Link <cpsubrian@gmail.com>
2015-02-09 22:00:00 -0800

    Fixes some panels stuff.

--- client/scripts/components/panel.js
+++ client/scripts/components/panel.js
@@ -13,8 +13,15 @@ var Panel = React.createClass({
     if (this.props.className) {
       classes.push(this.props.className);
     }
+
+    var style = {};
+    if (this.props.width) {
+      style.flex = '0 0';
+      style.flexBasis = this.props.width;
+    }
+
     return (
-      <div className={classes.join(' ')}>
+      <div className={classes.join(' ')} style={style}>
         {this.props.title ?
           <div className="panel-heading">
             <h3 className="panel-title">{this.props.title}</h3>
